1e019ac2541b42c3e2ffc24c6a03f125858602b4
RU-Prasath <prasathru14@gmail.com>
2025-05-26 09:47:01 +0530

    origin commit

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ const __dirname = path.dirname(__filename);
 app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 //middleware
-app.use(cors());
+app.use(cors({ origin: 'https://ruportfolio.vercel.app'}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
