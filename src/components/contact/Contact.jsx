727b6ec587ddf8a6cda9f67ad9cfc1adabe3d231
RU-Prasath <prasathru14@gmail.com>
2025-05-26 09:28:21 +0530

    new change

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
-      <h2>Contact Me</h2>
+      <h2>Ping Me</h2>
 
       <div className="container contact__container">
         <div className="contact__options">
