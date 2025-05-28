f9fc6a8d7ddf4e2f5d31122c5c645f8cec25e271
RU-Prasath <prasathru14@gmail.com>
2025-05-28 22:44:52 +0530

    btn component

--- client/src/components/common/Button.jsx
+++ client/src/components/common/Button.jsx
@@ -1,7 +1,7 @@
-export const Button = ({ text, type, onClick, className = '', style }) => {
+export const Button = ({ text, type, onClick, className = '', style, name }) => {
   return (
     <>
-        <button type={type} onClick={onClick} className={`btn ${className}`} style={style}>
+        <button type={type} onClick={onClick} className={`btn ${className}`} style={style} name={name}>
             {text}
         </button>
     </>
